ed6b093aacab0a9664257e3db62b82c829188c2a
shubham sharma <shubham@monteage.in>
2026-07-18 14:26:18 +0530

    t

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.myattendance.monteage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.0.15"
+        versionCode = 16
+        versionName = "1.0.16"
         multiDexEnabled = true
     }
 
